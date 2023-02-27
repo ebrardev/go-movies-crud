f3f5d8124a4d7052bdb29c93dee18e9c5ce9ea72
Ebrar <16155052@mersin.edu.tr>
2023-02-27 17:13:10 +0300

    syntax error

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"encoding/json"
-// 	"math/rand"
-// 	"net/http"
-// 	"strconv"
-// 	"github.com/gorilla/mux"
-// )
+ import (
+ 	"fmt"
+ 	"log"
+ 	"encoding/json"
+	"math/rand"
+ 	"net/http"
+ 	"strconv"
+	"github.com/gorilla/mux"
+ )
 
 type Movie struct {
 	ID       string    `json:"id"`
@@ -36,7 +29,56 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
-func main() {
+func deleteMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies= append(movies[:index], movies[index+1:]...)
+			break
+		}
+		 
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+ func getMovie(w http.ResponseWriter,r*http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+ }
+
+ func createMovie(w http.ResponseWriter, r*http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies,movie)
+	json.NewEncoder(w).Encode(movie)
+ }
+ func updateMovie(w http.ResponseWriter, r*http.Request) {
+
+	// set json  content type
+	w.Header().Set("Content-Type", "application/json")
+	// get params
+	params:= mux.Vars(r)
+	// loop through movies and find one with the id from the params
+	for index, item := range movies {
+		if  item.ID == params["id"] {
+			movies = append(movies[:index],movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies,movie)
+	json.NewEncoder(w).Encode(movie)
+	return
+	}
+ }
+   func main(){
 	r := mux.newRouter()
 
 	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
